b59df751e9a6bfbcd8545060d57ab5d78719e197
GABRIEL MORGANTI SANTELO FERREIRA <gabriel.msferreira@senacsp.edu.br>
2023-06-20 17:01:04 -0300

    Cadastro de produtos

---
+++ app/Models/Tamanho.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tamanho extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'tipos_produtos';
+    protected $primaryKey = 'id_tipo_produto';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    //Campos que podem ser visualizados/manipulados fora da classe
+    protected $fillable = [
+        'tipo'
+    ];
+}
+
